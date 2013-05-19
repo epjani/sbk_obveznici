3ee10a6d5341cc77714f7de93a231f6db6257dac
epjani <edin.pjanic@atlantbh.com>
2013-05-19 20:22:19 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SbkObveznici::Application.config.secret_token = 'a71d109a52f5e9b79073014df6b4390919ab5240ef72a71e8f419f44b58116e8c1772723beb33abe21e70cae0beaeb5c79fc0e529dbb26bf695c2f2e85c80c69'
